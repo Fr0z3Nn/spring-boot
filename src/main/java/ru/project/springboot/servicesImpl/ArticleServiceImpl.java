34f7a147a9cf758b8eb29d7e11efdcccac1539f2
Fr0z3Nn <luckyjohn057@gmail.com>
2020-10-28 20:16:09 +0300

    Добавлено: Исправлено: Имплементации сервисов вынесены в отдельный пакет. Удалено: ---------------------------

--- src/main/java/ru/project/springboot/servicesImpl/ArticleServiceImpl.java
+++ src/main/java/ru/project/springboot/servicesImpl/ArticleServiceImpl.java
@@ -1,16 +1,17 @@
-package ru.project.springboot.services;
+package ru.project.springboot.servicesImpl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.project.springboot.models.Article;
 import ru.project.springboot.models.User;
 import ru.project.springboot.repositories.ArticleRepository;
+import ru.project.springboot.services.ArticleService;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ArticleServiceImpl implements ArticleService{
+public class ArticleServiceImpl implements ArticleService {
     private final ArticleRepository articleRepository;
     @Override
     public List<Article> getAllArticles() {
--- src/main/java/ru/project/springboot/servicesImpl/UserServiceImpl.java
+++ src/main/java/ru/project/springboot/servicesImpl/UserServiceImpl.java
@@ -1,17 +1,18 @@
-package ru.project.springboot.services;
+package ru.project.springboot.servicesImpl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.project.springboot.models.Article;
 import ru.project.springboot.models.User;
 import ru.project.springboot.repositories.UserRepository;
+import ru.project.springboot.services.UserService;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
